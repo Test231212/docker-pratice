5de6f39d163bf3e7e910dddccff65e92bf0d272c
test231212 <j910219@gmail.com>
2024-06-11 14:12:11 +0900

    도커 연습

---
+++ ex03/entrypoint.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install -y git 1>/dev/null
+git clone https://github.com/busanuv/blog-last.git
+sleep 1s
+cd blog-last
+chmod +x gradlew
+./gradlew build
+java -jar -Dspring.profiles.active=dev build/libs/*.jar
+
